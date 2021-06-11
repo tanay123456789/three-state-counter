a8c6eeec0389c4702956b69120379d0b74a77db2
Tanay <tanaydwivedi456@gmail.com>
2021-06-11 20:46:41 +0530

    feature:added counter using useState hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import "./App.css";
 
 
 
 function App(){
+
+  const [count ,setCount]=useState(0)
+  
   return(
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+    <header>
+      <h1>Counter app using React</h1>
+    </header>
+      <h2>Current value of count is {count}</h2>
+      <button onClick={()=>setCount(0)}>Reset Counter</button>
+      <button onClick={()=>setCount(count+1)}>Increase Counter</button>
+      <button onClick={()=>setCount(count-1)}>Decrease Counter</button>
     </div>
   );
 }
